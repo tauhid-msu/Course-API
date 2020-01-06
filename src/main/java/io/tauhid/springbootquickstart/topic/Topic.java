ae21f0b5c8ec290abceeb1301c9e415e02dc1d0d
tauhid-msu <islama@msu.edu>
2020-01-06 18:33:35 -0500

    added embedded apache derby database

--- src/main/java/io/tauhid/springbootquickstart/topic/Topic.java
+++ src/main/java/io/tauhid/springbootquickstart/topic/Topic.java
@@ -1,7 +1,12 @@
 package io.tauhid.springbootquickstart.topic;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
 	
+	@Id
 	private String id;
 	private String name;
 	private String description;
--- src/main/java/io/tauhid/springbootquickstart/topic/TopicService.java
+++ src/main/java/io/tauhid/springbootquickstart/topic/TopicService.java
@@ -4,40 +4,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = new ArrayList<>(Arrays.asList(
-			new Topic("Spring","Spring Framework", "Spring Framework Description 3"),
-			new Topic("Java","Core Java", "Core Java Description 2"),
-			new Topic("Javascript","Javascript", "Javascript Description 1")
-		));
+	@Autowired
+	private TopicRepository topicRepository;
 	
 	public List<Topic> getAllTopics(){
-		return topics;
+		List<Topic> topics2 = new ArrayList<>();
+		topicRepository.findAll().forEach(topics2::add);
+		return topics2;
 	}
 	
 	public Topic getTopic(String id) {
-		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id).orElse(null);	
 	}
 
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
 	}
 	
 	public void updateTopic(String id, Topic topic) {
-		for(int i=0; i<topics.size(); i++) {
-			if(topics.get(i).getId().equals(id)) {
-				topics.set(i, topic);
-				return;
-			}
-		}
+		topicRepository.save(topic);
 	}
 
 	public void deleteTopic(String id) {
-		topics.removeIf(t->t.getId().equals(id));
+		topicRepository.deleteById(id);
 	}
 	
 
